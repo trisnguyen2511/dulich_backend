df29ccff79a5531c93ef570a606d2239bdb57f5d
trisnguyen2511 <75766468+trisnguyen2511@users.noreply.github.com>
2023-10-09 20:32:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { errorHandler } = require("./app/middlewares/ErrorHandler.js")
 app.use(express.json())
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "*",
         credentials: true,
     })
     // cors()
